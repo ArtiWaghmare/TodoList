059992bb82dab5b1454da30509f1194ee2f372fe
Arti <artiw362@gmail.com>
2024-02-26 18:52:32 +0530

    Updated

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,3 @@
-
-
-
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-
-// function Todolist(props) {
-//   return (
-//     <li className="list-item">
-//     <input
-//       type="checkbox"
-//       checked={props.checked}
-//       onChange={() => props.handleCheckboxChange(props.index)}
-//       className="checkbox"
-//     />
-//     <span className="space-between">
-//       {props.item}
-//     </span>
-//     <span className='icons'>
-//       <FontAwesomeIcon icon={faTrashAlt} className="icon-delete" onClick={() => props.deleteItem(props.index)} />
-//     </span>
-//   </li>
-//   );
-// }
-
-// export default Todolist;
-
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
